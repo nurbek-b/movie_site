aa7f91669f4936f6fece0b730f1e43f66af71778
nurbek-b <foto.nurbek@gmail.com>
2020-03-05 16:42:53 +0600

    Comment ot comment has done

--- movies/models.py
+++ movies/models.py
@@ -74,6 +74,9 @@ class Movie(models.Model):
     def get_absolute_url(self):
         return reverse('movie_detail', kwargs={'slug': self.url})
 
+    def get_review(self):
+        return self.reviews_set.filter(parent__isnull=True)
+
     class Meta:
         verbose_name = "Фильм"
         verbose_name_plural = "Фильмы"
@@ -125,7 +128,7 @@ class Reviews(models.Model):
     email = models.EmailField()
     name = models.CharField("Имя",  max_length=100)
     text = models.TextField("Сообщение", max_length=5000)
-    parent = models.ForeignKey('self', verbose_name="", on_delete=models.SET_NULL,
+    parent = models.ForeignKey('self', verbose_name="Родитель", on_delete=models.SET_NULL,
                                blank=True, null=True)
     movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE)
 
