d3b3ae8d813120f477f2ad159ca69b8ee8fbf262
nurbek-b <foto.nurbek@gmail.com>
2020-03-08 14:41:42 +0600

    Actor detail view and related name is realized

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', MoviesView.as_view(), name='movie_list'),
     path('<slug:slug>/', MovieDetail.as_view(), name='movie_detail'),
     path('review/<int:pk>/', AddReview.as_view(), name='add_review'),
+    path('actor/<str:slug>/', ActorView.as_view(), name='actor_detail'),
 ]
