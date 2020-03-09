d45b8bd0a891e7a29413ecd6f6cac03367eccb3f
nurbek-b <foto.nurbek@gmail.com>
2020-03-09 12:05:56 +0600

    Search is done

--- movies/views.py
+++ movies/views.py
@@ -109,3 +109,16 @@ class AddStarRating(View):
             return HttpResponse(status=201)
         else:
             return HttpResponse(status=400)
+
+
+class Search(ListView):
+    """Поиск для фильмов"""
+
+    paginate_by = 2
+    def get_queryset(self):
+        return Movie.objects.filter(title__contains=self.request.GET.get('q'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['q'] = f'q={self.request.GET.get("q")}&'
+        return context
