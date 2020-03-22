392bcf05bcadb22bada5faf6b2c845316282b713
nurbek-b <foto.nurbek@gmail.com>
2020-03-22 13:23:10 +0600

    allauth registration done

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -46,7 +46,10 @@ INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
     'contact',
 
-    'snowpenguin.django.recaptcha3'
+    'snowpenguin.django.recaptcha3',
+    
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -93,6 +96,12 @@ DATABASES = {
 }
 
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -124,6 +133,14 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
 
 
 gettext = lambda s: s
